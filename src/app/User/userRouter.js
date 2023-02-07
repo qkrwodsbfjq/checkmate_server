4f9409798aa479a7999fc0a8c8d8e4251ffe0c2c
qkrwodsbfjq <qkrwodbsfj@naver.com>
2023-02-07 21:53:00 +0900

    test..

--- src/app/User/userRouter.js
+++ src/app/User/userRouter.js
@@ -2,10 +2,10 @@ module.exports = function(app){
     const user = require('./userController');
     const jwtMiddleware = require('../../../config/jwtMiddleware');
  
-    app.get('/user/:userId', user.getProfile);
-    app.patch('/user/:userId/nickname', user.modifyNickname);
-    app.patch('/user/:userId/secession', user.deleteAccount);
-    app.patch('/user/:userId/acceptance', user.modifyAcceptanceStatus);
+    app.get('/user/:userId', jwtMiddleware, user.getProfile);
+    app.patch('/user/:userId/nickname', jwtMiddleware, user.modifyNickname);
+    app.patch('/user/:userId/secession', jwtMiddleware, user.deleteAccount);
+    app.patch('/user/:userId/acceptance', jwtMiddleware, user.modifyAcceptanceStatus);
 
     app.get('/app/auto-login', jwtMiddleware, user.check);
 }
